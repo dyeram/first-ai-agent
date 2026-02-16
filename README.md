b05e8c5a57543414ed227eb04e3d7ff98ab0098b
dyeram <dyer.alexander@protonmail.com>
2026-02-16 15:02:51 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Basic AI agent 
+
+A basic AI agent that makes API calls to an LLM model.
+
+## Overview
+- LLM model: gemini-2.5-flash
---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+def main():
+    # get API key
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("API key is missing from env.")
+
+    # call LLM Model (gemini-2.5-flash model)
+    client = genai.Client(api_key = api_key)
+    response = client.models.generate_content(
+    model='gemini-2.5-flash',
+    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+
+    # print output
+    print("Hello from first-ai-agent!")
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
