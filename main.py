00a935186ed9b908c5ac0e6ee9f0538e4b67ae18
dyeram <dyer.alexander@protonmail.com>
2026-02-16 16:37:27 +0100

    added user input

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
 def main():
-    # get API key
+    # Setup argument parser
+    parser = argparse.ArgumentParser(description = 'AI chatbot')
+    parser.add_argument("user_prompt", type = str, help = "User prompt")
+    args = parser.parse_args()
+
+    # load API key from .env
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key is None:
-        raise RuntimeError("API key is missing from env.")
+        raise RuntimeError("GEMINI_API_KEY environmental variable is missing.")
 
     # call LLM Model (gemini-2.5-flash model)
     client = genai.Client(api_key = api_key)
     response = client.models.generate_content(
     model='gemini-2.5-flash',
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    contents=args.user_prompt
     )
 
+    # Verify successful API request
+    if response.usage_metadata is None:
+        raise RuntimeError("API request failed")
+    
     # print output
-    print("Hello from first-ai-agent!")
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.prompt_token_count}") 
     print(response.text)
 
 if __name__ == "__main__":
