e167461eb23a7767258207c04fe46e0e1e853216
dyeram <dyer.alexander@protonmail.com>
2026-02-16 17:14:02 +0100

    modified user input functions

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 import argparse
+
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     # Setup argument parser
@@ -9,25 +11,30 @@ def main():
     parser.add_argument("user_prompt", type = str, help = "User prompt")
     args = parser.parse_args()
 
-    # load API key from .env
+    # Load API key from .env
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key is None:
         raise RuntimeError("GEMINI_API_KEY environmental variable is missing.")
-
-    # call LLM Model (gemini-2.5-flash model)
+    
+    # Define inputs and call LLM (via Google's genai)
     client = genai.Client(api_key = api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    generate_content(client, messages)
+
+
+# Generate LLM response (via Google's genai)
+def generate_content(client, messages):
     response = client.models.generate_content(
-    model='gemini-2.5-flash',
-    contents=args.user_prompt
+        model='gemini-2.5-flash',
+        contents=messages
     )
 
     # Verify successful API request
     if response.usage_metadata is None:
         raise RuntimeError("API request failed")
     
-    # print output
-    print(f"User prompt: {args.user_prompt}")
+    # Print LLM response
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.prompt_token_count}") 
     print(response.text)
