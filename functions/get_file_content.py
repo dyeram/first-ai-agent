daca386a997f06a38190a0228e33938a28c6a5d6
dyeram <dyer.alexander@protonmail.com>
2026-02-20 19:51:29 +0100

    fix(functions): improved error handling for functions

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -4,28 +4,28 @@ import os.path
 from config import CHAR_LIMIT
 
 def get_file_content(working_directory, file_path):
-    # Setup target_file_path
-    working_directory_abs = os.path.abspath(working_directory)
-    target_file = os.path.normpath(os.path.join(working_directory_abs, file_path))
-
-    # is file_path in working_directory? (True/False)
-    valid_target_file = os.path.commonpath([working_directory_abs, target_file]) == working_directory_abs
-    if not valid_target_file:
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    
-    # is file_path a file?
-    if not os.path.isfile(target_file):
-        return f'Error: File not found or is not a regular file: "{file_path}"'
-    
-    # get contents of target file (as single string up to CHAR_LIMIT)
     try:
+        # Setup target_file_path
+        working_directory_abs = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(working_directory_abs, file_path))
+
+        # is file_path in working_directory? (True/False)
+        valid_target_file = os.path.commonpath([working_directory_abs, target_file]) == working_directory_abs
+        if not valid_target_file:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        
+        # is file_path a file?
+        if not os.path.isfile(target_file):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        
+        # get contents of target file (as single string up to CHAR_LIMIT)
         with open(target_file, "r") as f:
             content = f.read(CHAR_LIMIT)
-        
-            # If there's a character at CHAR_LIMIT + 1
+            
+                # If there's a character at CHAR_LIMIT + 1
             if f.read(1):
-                content += f'[...File "{file_path}" truncated at {CHAR_LIMIT} characters]'
-        
+                    content += f'[...File "{file_path}" truncated at {CHAR_LIMIT} characters]'
+            
         return content
     
     except Exception as e:
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,21 +3,21 @@
 import os.path
 
 def get_files_info(working_directory, directory="."):
-    # Setup target directory
-    working_directory_abs = os.path.abspath(working_directory)
-    target_dir = os.path.normpath(os.path.join(working_directory_abs, directory))
-
-    # is target_dir in working_directory? (True/False)
-    valid_target_directory = os.path.commonpath([working_directory_abs, target_dir]) == working_directory_abs
-    if not valid_target_directory:
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    
-    # is target_dir an existing directory?
-    if not os.path.isdir(target_dir):
-        return f'Error: "{directory}" is not a directory'
-
-    # get contents of target directory (as single string)
     try:
+        # Setup target directory
+        working_directory_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_directory_abs, directory))
+
+        # is target_dir in working_directory? (True/False)
+        valid_target_directory = os.path.commonpath([working_directory_abs, target_dir]) == working_directory_abs
+        if not valid_target_directory:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        
+        # is target_dir an existing directory?
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+
+        # get contents of target directory (as single string)
         return get_dir_contents(target_dir)
     except Exception as e:
         return f"Error: {e}"
