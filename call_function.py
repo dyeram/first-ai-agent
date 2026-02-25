bf3a187327d9d02b255024aa3dd05b489e84d08d
dyeram <dyer.alexander@protonmail.com>
2026-02-25 11:50:06 +0100

    feat(call_function): creates a Tool object that provides a list of function schemas to LLM

---
+++ call_function.py
@@ -0,0 +1,8 @@
+# Creates a Tool object that provides a list of functions that the LLM can use to generate a reponse.
+# Requires: a FunctionDeclaration object (schema) for each function.
+
+from functions.get_files_info import schema_get_files_info
+
+available_functions = types.Tool(
+    function_declarations=[schema_get_files_info],
+)
