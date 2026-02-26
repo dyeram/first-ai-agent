0a45089e4fc17e3c876eb1541c235bd2f95ef9f2
dyeram <dyer.alexander@protonmail.com>
2026-02-26 23:20:12 +0100

    wip(prompts): updated system prompts to include additional functions

--- prompts.py
+++ prompts.py
@@ -2,9 +2,15 @@
 system_prompt = """
 You are a helpful AI coding agent.
 
-When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+When a user asks a question or makes a request, first generate a function call plan.
+You may perform the following operations within system-defined working directories and their subdirectories:
 
-- List files and directories
+- List files and subdirectories, along with their metadata.
+- Retrieve the contents of specified files.
+- Create or overwrite specified files with the provided content.
+- Execute a specified Python file, optionally passing command-line arguments, and returning its output.
 
-All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+All paths you provide should be relative to the system-defined working directory.
+The working directory is automatically injected into your function calls and must not be specified explicitely.
+No operations outside the system-defined working directories are allowed.
 """
